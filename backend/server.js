2472e50d8032636a14b9cf7534fdc18ca2f97506
Parth chaturvedi <phlpharma556@gmail.com>
2026-08-06 18:09:40 +0530

    added production env

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,10 @@ connectDB();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://agri-twin-ai-87pc.vercel.app/login"
+    ],
     credentials: true,
   })
 );
